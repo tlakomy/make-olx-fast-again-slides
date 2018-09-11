f67df2d7f1a732ee0d82beb85ba9bf793eece756
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2018-09-11 12:45:33 +0200

    Slides vol. 2

--- presentation/index.js
+++ presentation/index.js
@@ -10,6 +10,7 @@ import {
     Heading,
     ListItem,
     List,
+    Magic,
     Image,
     Quote,
     Slide,
@@ -44,7 +45,21 @@ const images = {
     glacier: require('../assets/glacier.png'),
     devtools: require('../assets/devtools.png'),
     radar: require('../assets/radar.png'),
-    thinking: require('../assets/thinking.jpg')
+    lato: require('../assets/lato.png'),
+    thinking: require('../assets/thinking.jpg'),
+    sevenhours: require('../assets/sevenhours.png'),
+    tracking: require('../assets/tracking.jpg'),
+    managers: require('../assets/managers.jpg'),
+    hamsters: require('../assets/hamsters.gif'),
+    tumbleweed: require('../assets/tumbleweed.jpg'),
+    floppy: require('../assets/floppy.jpg'),
+    tracks: require('../assets/tracks.gif'),
+    tracks2: require('../assets/tracks2.gif'),
+    epic: require('../assets/epic.png'),
+    whoops: require('../assets/whoops.png'),
+    lte_modem: require('../assets/lte_modem.png'),
+    cost: require('../assets/cost.png'),
+    newrelic: require('../assets/newrelic.png')
 };
 
 export default class Presentation extends React.Component {
@@ -242,18 +257,29 @@ export default class Presentation extends React.Component {
                     </Heading>
                     <Cite>http://q2s.tastyspleen.net/</Cite>
                 </Slide>
+                <Slide>
+                    <Image
+                        fit
+                        bgColor="primary"
+                        height="700px"
+                        src={images.lte_modem.replace('/', '')}
+                        margin={0}
+                    />
+                </Slide>
+                <Slide>
+                    <Image
+                        fit
+                        bgColor="primary"
+                        src={images.cost.replace('/', '')}
+                        margin={0}
+                    />
+                </Slide>
                 <Slide
                     bgImage={images.thinking.replace('/', '')}
                     bgDarken={0.6}
                 >
                     <Heading fit textColor="white">
                         So what was going on?
-                    </Heading>
-                </Slide>
-                <Slide>
-                    <Heading fit>
-                        Let's go through some of the more <i>interesting</i>{' '}
-                        issues
                     </Heading>
                 </Slide>
                 <Slide>
@@ -316,6 +342,201 @@ export default class Presentation extends React.Component {
                 </Slide>
                 <Slide bgImage={images.radar.replace('/', '')} />
                 <Slide>
+                    <Heading>JavaScript</Heading>
+                </Slide>
+                <Slide>
+                    <Heading fit>
+                        Most of our JS comes from external vendors
+                    </Heading>
+                    <List>
+                        <Appear>
+                            <ListItem>Tracking</ListItem>
+                        </Appear>
+                        <Appear>
+                            <ListItem>GTM</ListItem>
+                        </Appear>
+                        <Appear>
+                            <ListItem>Ads</ListItem>
+                        </Appear>
+                        <Appear>
+                            <ListItem>1.5MB per page load</ListItem>
+                        </Appear>
+                    </List>
+                </Slide>
+                <Slide
+                    bgImage={images.tracking.replace('/', '')}
+                    bgDarken={0.75}
+                >
+                    <Heading textColor="white">
+                        Do we need all that tracking? 🤔
+                    </Heading>
+                </Slide>
+                <Slide>
+                    <Heading>Fonts</Heading>
+                </Slide>
+                <Slide>
+                    <Heading fit>
+                        <span style={{ color: 'white' }}>~950kB</span> in fonts
+                        per page view
+                    </Heading>
+                </Slide>
+                <Slide>
+                    <Image
+                        fit
+                        bgColor="secondary"
+                        src={images.lato.replace('/', '')}
+                        height={'200px'}
+                        margin={0}
+                    />
+                </Slide>
+                <Slide>
+                    <Heading>Okay, how do we address those issues?</Heading>
+                </Slide>
+                <Slide>
+                    <Heading fit>
+                        Start a new channel on slack, of course!
+                    </Heading>
+                </Slide>
+                <Slide>
+                    <Heading fit>#make-olx-fast-again</Heading>
+                </Slide>
+                <Slide bgImage={images.sevenhours.replace('/', '')} />
+                <Slide bgImage={images.tumbleweed.replace('/', '')} />
+                <Slide bgImage={images.managers.replace('/', '')} />
+                <Slide>
+                    <Image
+                        fit
+                        bgColor="secondary"
+                        src={images.hamsters.replace('/', '')}
+                        height={'400px'}
+                        margin={0}
+                    />
+                </Slide>
+                <Slide>
+                    <Heading fit>
+                        We have support from tech, product and marketing
+                    </Heading>
+                </Slide>
+                <Slide>
+                    <Heading fit>We even got access to GTM 😎</Heading>
+                </Slide>
+                <Slide>
+                    <Heading>And we've been busy!</Heading>
+                </Slide>
+                <Slide>
+                    <Image
+                        fit
+                        bgColor="secondary"
+                        src={images.epic.replace('/', '')}
+                        height={'650px'}
+                        margin={0}
+                    />
+                </Slide>
+                <Slide>
+                    <Appear>
+                        <Heading fit>
+                            Removing unnecessary trackers and GTM
+                        </Heading>
+                    </Appear>
+                    <br />
+                    <br />
+                    <Appear>
+                        <Heading fit>Kudos for Jeremy Castan 🎉</Heading>
+                    </Appear>
+                </Slide>
+                <Slide>
+                    <Heading>Result?</Heading>
+                </Slide>
+                <Slide bgImage={images.floppy.replace('/', '')} bgDarken={0.6}>
+                    <Heading textColor="white">
+                        OLX.pl homepage can now fit on a floppy drive
+                    </Heading>
+                </Slide>
+                <Slide>
+                    <Heading>Results</Heading>
+                    <List>
+                        <Appear>
+                            <ListItem>
+                                Homepage bundle is{' '}
+                                <span style={{ color: 'white' }}>51.8%</span>{' '}
+                                smaller
+                            </ListItem>
+                        </Appear>
+                        <Appear>
+                            <ListItem>
+                                Listing bundle is{' '}
+                                <span style={{ color: 'white' }}>29.6%</span>{' '}
+                                smaller
+                            </ListItem>
+                        </Appear>
+                        <Appear>
+                            <ListItem>
+                                Ad page is{' '}
+                                <span style={{ color: 'white' }}>~20%</span>{' '}
+                                smaller
+                            </ListItem>
+                        </Appear>
+                    </List>
+                </Slide>
+                <Slide>
+                    <Heading>Next step</Heading>
+                </Slide>
+                <Slide>
+                    <Heading>Tackling high JS error rate</Heading>
+                </Slide>
+                <Slide>
+                    <Heading>
+                        On some countries it's{' '}
+                        <span style={{ color: 'white' }}>~25%</span>
+                    </Heading>
+                </Slide>
+                <Slide>
+                    <Image
+                        fit
+                        bgColor="secondary"
+                        src={images.newrelic.replace('/', '')}
+                        height={'500px'}
+                        margin={0}
+                    />
+                </Slide>
+                <Slide>
+                    <Image
+                        fit
+                        bgColor="secondary"
+                        src={images.tracks.replace('/', '')}
+                        height={'650px'}
+                        margin={0}
+                    />
+                </Slide>
+                <Slide>
+                    <Image
+                        fit
+                        bgColor="secondary"
+                        src={images.tracks2.replace('/', '')}
+                        height={'650px'}
+                        margin={0}
+                    />
+                </Slide>
+                <Slide>
+                    <Heading>It's hard but it's worth it</Heading>
+                </Slide>
+                <Slide>
+                    <Heading>
+                        Otherwise you might{' '}
+                        <span style={{ color: 'white' }}>miss</span> an
+                        important issue on prod
+                    </Heading>
+                </Slide>
+                <Slide>
+                    <Heading>Who needs a fast website when it's broken</Heading>
+                </Slide>
+                <Slide bgImage={images.whoops.replace('/', '')} />
+                <Slide>
+                    <Heading>
+                        We own the experience we provide to our users
+                    </Heading>
+                </Slide>
+                <Slide>
                     <Heading>Key takeaways</Heading>
                     <List>
                         <Appear>
@@ -328,13 +549,21 @@ export default class Presentation extends React.Component {
                         </Appear>
                         <Appear>
                             <ListItem>
+                                Focus on low hanging fruits first
+                            </ListItem>
+                        </Appear>
+                        <Appear>
+                            <ListItem>
                                 Pineapple does not belong on a pizza
                             </ListItem>
                         </Appear>
                         <Appear>
-                            <ListItem>Cooperation is key</ListItem>
+                            <ListItem>Have a plan</ListItem>
                         </Appear>
                     </List>
+                </Slide>
+                <Slide>
+                    <Heading fit>Join #make-olx-fast-again on slack</Heading>
                 </Slide>
                 <Slide>
                     <Heading>Thank you!</Heading>
